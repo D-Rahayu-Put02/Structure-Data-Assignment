0338051515e7b5ee6e7c4178ab19a3c6cce0a3b5
D-Rahayu-Put02 <dwirahayuputra17@gmail.com>
2025-10-07 21:53:22 +0700

    Create hitung.cpp

---
+++ modul 1/hitung.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    const float pi = 3.14;
+
+    cout << "masukan angka: ";
+    cin >> n;
+
+    cout << "ankga dikeluarkan: " << n << endl;
+    cout << "nilai konstanta pi: " << pi << endl;
+    return 0;
+
+}
