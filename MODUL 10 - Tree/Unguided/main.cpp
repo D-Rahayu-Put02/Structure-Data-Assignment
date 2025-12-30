3f6f42834aa8d5d0dd771c810a8cc5629ea7dea5
D-Rahayu-Put02 <dwirahayuputra17@gmail.com>
2025-12-30 07:16:53 +0700

    Implement basic binary search tree operations

--- MODUL 10 - Tree/Unguided/main.cpp
+++ MODUL 10 - Tree/Unguided/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include "bstree.h"
 
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    address root = NIL;
+
+    insertNode(root, 1);
+    insertNode(root, 2);
+    insertNode(root, 6);
+    insertNode(root, 4);
+    insertNode(root, 5);
+    insertNode(root, 3);
+    insertNode(root, 6); // duplikat, tidak masuk
+    insertNode(root, 7);
+
+    InOrder(root);
+    cout << endl;
+
+    cout << "kedalaman : " << hitungKedalaman(root, 0) << endl;
+    cout << "jumlah node : " << hitungJumlahNode(root) << endl;
+    cout << "total : " << hitungTotalInfo(root) << endl;
+
+    return 0;
+}
