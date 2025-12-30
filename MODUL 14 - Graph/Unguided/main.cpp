4d90ca13a537976c93eb82c8eee8ef16735e3978
D-Rahayu-Put02 <dwirahayuputra17@gmail.com>
2025-12-30 17:26:59 +0700

    Implement graph creation and traversal methods
    Initialize a graph, insert nodes, and connect them. Implement DFS and BFS traversal starting from node 'A'.

--- MODUL 14 - Graph/Unguided/main.cpp
+++ MODUL 14 - Graph/Unguided/main.cpp
@@ -1,1 +1,42 @@
+#include "graph.h"
 
+int main() {
+    Graph G;
+    createGraph(G);
+
+    insertNode(G, 'A');
+    insertNode(G, 'B');
+    insertNode(G, 'C');
+    insertNode(G, 'D');
+    insertNode(G, 'E');
+    insertNode(G, 'F');
+    insertNode(G, 'G');
+    insertNode(G, 'H');
+
+    connectNode(findNode(G,'A'), findNode(G,'B'));
+    connectNode(findNode(G,'A'), findNode(G,'C'));
+    connectNode(findNode(G,'B'), findNode(G,'D'));
+    connectNode(findNode(G,'B'), findNode(G,'E'));
+    connectNode(findNode(G,'C'), findNode(G,'F'));
+    connectNode(findNode(G,'C'), findNode(G,'G'));
+    connectNode(findNode(G,'D'), findNode(G,'H'));
+    connectNode(findNode(G,'E'), findNode(G,'H'));
+    connectNode(findNode(G,'F'), findNode(G,'H'));
+    connectNode(findNode(G,'G'), findNode(G,'H'));
+
+    cout << "DFS : ";
+    printDFS(G, findNode(G,'A'));
+    cout << endl;
+
+    adrNode P = G.first;
+    while (P != NULL) {
+        P->visited = 0;
+        P = P->next;
+    }
+
+    cout << "BFS : ";
+    printBFS(G, findNode(G,'A'));
+    cout << endl;
+
+    return 0;
+}
